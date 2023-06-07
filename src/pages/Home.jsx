75791d069024d9353343506e32430dcd23d33822
Eduard Antoshchuk <e.antoshchuk@gmail.com>
2023-06-08 01:38:17 +0300

    home-started

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import fetchTrendMovies from 'Services/FetchTrendingMovies-api';
+
 const Home = () => {
-  return <div>Home page</div>;
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetchTrendMovies()
+      .then(res => {
+        return setTrendingMovies([res]);
+      })
+      .catch(err => toast.warn(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <>
+      <ToastContainer autoClose={3000} theme="colored" />
+      <div>Home page</div>;
+      <ul>
+        <li>{trendingMovies}</li>
+      </ul>
+    </>
+  );
 };
 
 export default Home;
