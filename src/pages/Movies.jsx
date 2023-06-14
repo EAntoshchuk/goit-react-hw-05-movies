76ae247c4057eacd9fe2cdfc287eeefa8fd7313b
Eduard Antoshchuk <e.antoshchuk@gmail.com>
2023-06-14 03:02:32 +0300

    search-not-working

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -3,19 +3,21 @@ import MagnifyingGlassLodaer from 'components/Loader/Loader';
 import { useEffect, useState } from 'react';
 import { Link, useLocation, useSearchParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { DebounceInput } from 'react-debounce-input';
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   const request = searchParams.get('request') ?? '';
+  const [query, setQuery] = useState('');
   // const [request, setRequest] = useState('');
   // const searchedMovies = movies.filter(movie => movie.includes(request));
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = evt => {
     evt.preventDefault();
-    const request = evt.target.search.value;
+    const request = evt.target.value.trim();
     console.log('searchrequest', request);
     if (request === '') {
       return toast.warn('Enter search request please');
@@ -46,10 +48,13 @@ const Movies = () => {
             <input
               type="text"
               name="search"
-              defaultValue={request}
+              valuealue={request}
               onChange={handleSubmit}
             />
-            <button onClick={() => setSearchParams({ request })}>
+            <button
+              type="submit"
+              onClick={() => setSearchParams({ request: request })}
+            >
               Search movie
             </button>
             {movies.map(
@@ -71,8 +76,18 @@ const Movies = () => {
               }) => {
                 return (
                   <li key={id}>
-                    <Link to={`${movies}`} state={{ from: location }}>
-                      {title}
+                    <Link to={`/movies/${id}`} state={{ from: location }}>
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                        alt={title}
+                      />
+
+                      <h4>
+                        {title}
+                        <p>Release date: {release_date.slice(0, -6)}</p>
+                      </h4>
+                      <h5>Rating: {vote_average}</h5>
+                      <h5>Votes: {vote_count}</h5>
                     </Link>
                   </li>
                 );
